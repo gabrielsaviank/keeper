e64938073ee17d708814c717bdd93652d4feb1d9
GabrielSavian <gabriel@amplimed.com.br>
2020-11-18 21:24:25 -0300

    screens created

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Note from './Note';
+import Footer from './Footer';
+
+
+const App = () => {
+    return(
+        <div>
+            <Header/>
+            <Note/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Note = () => {
+    return(
+        <div className="note">
+            <h1>This is the Note Title</h1>
+            <p>This is the note body</p>
+        </div>
+    );
+};
+
+export default Note;
